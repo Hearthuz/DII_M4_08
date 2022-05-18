850a0cb05f9f3f8eb24058b38b6688cf49befb1b
ariya <ariya_watchara@cmu.ac.th>
2022-05-18 15:33:10 +0700

    event type

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
-var name = 'Heart'
-document.getElementById('no').innerHTML = "<b><i>"+name+"</i></b>"
-document.getElementById('ok').addEventListener('click', function(e){
-    alert('ok is clicked')
-    console.log(e)
-})
+// document.getElementById('cancel').addEventListener('mousemove', function(e){
+//     console.log("X = ", e.screenX)
+//     console.log("Y = ", e.screenY)
+// })
+let elems = document.getElementsByClassName('flex-item')
+console.log(elems)
+let x = 5
+for(let elem of elems){
+    elem.addEventListener('mousemove', function(e){
+        console.log("x = ", e.screenX, "y = ", e.screenY)
+        let x = 20
+        console.log(x)
+    })
+    elem.addEventListener('click', function(e){
+        alert(elem.innerText)
+        console.log('this is x from click', x)
+    })
+}
