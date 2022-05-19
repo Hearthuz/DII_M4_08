d7089b828e509c16806d82e4f268b95052295d23
ariya <ariya_watchara@cmu.ac.th>
2022-05-19 15:25:57 +0700

    dom node

--- script.js
+++ script.js
@@ -1,109 +1,16 @@
-// document.getElementById('cancel').addEventListener('mousemove', function(e){
-//     console.log("X = ", e.screenX)
-//     console.log("Y = ", e.screenY)
-// })
-// let elems = document.getElementsByClassName('flex-item')
-// console.log(elems)
-// let x = 5
-// for(let elem of elems){
-//     elem.addEventListener('mousemove', function(e){
-//         console.log("x = ", e.screenX, "y = ", e.screenY)
-//         let x = 20
-//         console.log(x)
-//     })
-//     elem.addEventListener('click', function(e){
-//         console.log('this is x from click', x)
-//     })
-// }
-// ok.addEventListener('click', function (e) {
-//     let noElement = document.getElementById('no'); 
-//     if (noElement.classList.contains('toggleOn')) { 
-//         noElement.classList.replace('toggleOn', 'toggleOff'); 
-//     } 
-//     else if(noElement.classList.contains('toggleOff')) { 
-//         noElement.classList.replace('toggleOff', 'toggleOn'); 
-//     } 
-//     else { 
-//         noElement.classList.add('toggleOn'); 
-//     }
-// });
-// let margin = 20
-// ok.addEventListener('click', function(e){
-//     margin += 5;
-//     var marginSize = `20px ${margin}px 20px ${margin}px`
-//     document.getElementById('cancel').style.margin = marginSize
-//     document.getElementById('no').style.margin = marginSize
-// })
-// var borderTick = 5
-// document.getElementById('cancel').style.border = `${borderTick}px solid white`
+var searchButton = document.getElementById('searchButton')
+var inputText = document.getElementById('inputText')
+var output = document.getElementById('output')
 
-// let showText = ['ok','วิชานี้','ง่าย','จริงๆ นะจ้ะ']
-// let showIndex = 0
-// document.getElementById('no').addEventListener('dblclick', function(e){
-//     showIndex++
-//     if(showIndex >= showText.length){
-//         showIndex = 0;
-//     }
-//     console.log('show index = ${showIndex}')
-//     const okButton = document.getElementById('ok')
-//     okButton.innerText = showText[showIndex]
-//     // const elem = document.getElementById('ok'); 
-//     // if (elem.innerText === 'ok') { 
-//     //     elem.innerText = 'วิชานี้'
-//     // } 
-//     // else if(elem.innerText === 'วิชานี้') { 
-//     //     elem.innerText = 'ง่าย'
-//     // } 
-//     // else if (elem.innerText === 'ง่าย') { 
-//     //     elem.innerText = 'จริงๆ นะจ้ะ'  
-//     // }
-//     // else if (elem.innerText === 'จริงๆ นะจ้ะ') { 
-//     //     elem.innerText = 'ok'  
-//     // }
-// })
-// let output = ''
-// let outputElements = document.getElementById('output')
-// function onOKClicked(e){
-//     e.stopPropagation();
-    // alert('ok clicked')
-    // output += 'OK'
-    // outputElements.innerText = output
-//     addText('OK')
-// }
-// function onCancelClicked(e){
-//     e.stopPropagation();
-    // alert(' cancel clicked')
-    // output += 'Cancel'
-    // outputElements.innerText = output
-//     addText('Cancel')
-// }
-// function onNoClicked(e){
-//     e.stopPropagation();
-    // alert(' no clicked')
-    // output += 'No'
-    // outputElements.innerText = output
-//     addText('No')
-// }
-// document.getElementById('cancel').addEventListener('click',onCancelClicked)
-// document.getElementById('no').onclick = onNoClicked
-// document.getElementById('container').onclick = function(e){
-//     alert('container clicked')
-// }
-// document.getElementById('container').onclick = function(){
-//     output = ''
-//     outputElements.innerText = output
-// }
-var outputContainer = document.getElementById('output-container')
-var counter = 0;
-function addNode(){
-    newNode = document.createElement('div')
-    newNode.classList.add('flex-item')
-    newNode.setAttribute('id', counter)
-    newNode.innerText = counter
-    counter = counter+1
-    newNode.innerText = counter
-    outputContainer.appendChild(newNode)
+function addText(){
+    let text = inputText.value
+    console.log(text)
+    let newButton = document.createElement('button')
+    newButton.classList.add('btn')
+    newButton.classList.add('btn-outline-primary')
+    newButton.setAttribute('type','button')
+    newButton.innerText = text
+    output.appendChild(newButton)
 }
-function onOKClicked(e){
-    addNode()
-}
+inputText.addEventListener('blur',addText)
+searchButton.addEventListener('click',addText)
